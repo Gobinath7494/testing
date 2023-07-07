c3cc4f2f8c46fbe846a12e13e27d7b1a573ff5ea
Gobinath7494 <134025947+Gobinath7494@users.noreply.github.com>
2023-07-07 14:42:12 +0530

    testing

---
+++ index.js
@@ -0,0 +1,11 @@
+// $(".cover").on("click", function () {
+//   $(this).children().css({
+//     "z-index": 1,
+//     opacity: 1,
+//   });
+//   $(this).children().trigger("play");
+// });
+
+// $("video").on("click", function () {
+//   console.log("a");
+// });
